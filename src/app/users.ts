609641133e641ddf25d5b3bb9daebc42f3829a49
efeagca <efe.agca@hotmail.com>
2021-01-19 16:00:03 +0300

    First Commit

---
+++ src/app/users.ts
@@ -0,0 +1,4 @@
+export const users = [
+    {name: "Ferhat", surname:"Erata", email:"erata@yale.edu", password:"gokcinenes"},
+    {name: "Efe", surname:"Agca", email:"ee@ee.ee", password:"1234"}
+]
