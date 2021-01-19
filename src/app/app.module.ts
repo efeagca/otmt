734452a4477c01ab17b661f60fa100512a3f6371
gokcin-sezgin <gokcin_sezgin@outlook.com>
2021-01-19 17:48:47 +0300

    new project screen added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { NewprojectComponent } from './newproject/newproject.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    NewprojectComponent
   ],
   imports: [
     BrowserModule,
